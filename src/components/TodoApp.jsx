51cb25aa9b85e67a8195eb7b68f8b3a400010c16
Truong Thien Phuc <truongthienphuc6655@gmail.com>
2026-03-23 21:47:13 +0700

    Todo List

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import Task from "./Task";
+
+export const STATUS = {
+  DONE: 1,
+  PENDING: 2,
+};
+
+export default function TodoApp() {
+  const [tasks, setTasks] = useState([]);
+
+  const [newTask, setNewTask] = useState("");
+
+  const [editTask, setEditTask] = useState(null);
+
+  const [error, setError] = useState(null);
+
+  const handleChangeNewTask = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!newTask) {
+      setError("New task cannot be empty!");
+      return;
+    }
+
+    setError(null);
+
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      {
+        id: Math.ceil(Math.random() * 1000000),
+        name: newTask,
+        status: STATUS.PENDING,
+      },
+    ]);
+
+    setNewTask("");
+  };
+
+  const handleUpdateTask = () => {
+    if (!newTask) {
+      setError("New task cannot be empty!");
+      return;
+    }
+
+    setError(null);
+
+    setTasks((prevTasks) =>
+      prevTasks.map((task, index) => {
+        if (task.id === editTask.id) {
+          task.name = newTask;
+        }
+
+        return task;
+      })
+    );
+
+    setEditTask(null);
+
+    setNewTask("");
+  };
+
+  const handleUpdateStatus = (e, id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task, index) => {
+        const newStatus = e.target.checked ? STATUS.DONE : STATUS.PENDING;
+
+        if (task.id === id) {
+          task.status = newStatus;
+        }
+
+        return task;
+      })
+    );
+  };
+
+  const handleEditTask = (id) => {
+    const editTask = tasks.find((task) => task.id === id);
+
+    setEditTask(editTask);
+
+    setNewTask(editTask.name);
+  };
+
+  const handleDeleteTask = (id) => {
+    if (editTask?.id === id) {
+      setEditTask(null);
+
+      setNewTask("");
+    }
+
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+  };
+
+  return (
+    <>
+      <div className="todo-app">
+        <div className="todo-box">
+          <h1 className="title">Todo List App</h1>
+          <div className="create-box">
+            <input
+              type="text"
+              value={newTask}
+              onChange={handleChangeNewTask}
+              name="name"
+              placeholder="Add your task"
+            />
+
+            {editTask ? (
+              <button onClick={handleUpdateTask} className="btn-add-task">
+                🗸
+              </button>
+            ) : (
+              <button onClick={handleSubmit} className="btn-add-task">
+                +
+              </button>
+            )}
+          </div>
+          {error && <div className="error">{error}</div>}
+          {tasks.length === 0 && (
+            <div className="empty-list">
+              Your list is empty! <br />
+              Add a new task above to get started.
+            </div>
+          )}
+          <div className="todo-list">
+            {tasks.map((task, index) => (
+              <Task
+                key={index}
+                task={task}
+                onChangeStatus={handleUpdateStatus}
+                onEdit={handleEditTask}
+                onDelete={handleDeleteTask}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
